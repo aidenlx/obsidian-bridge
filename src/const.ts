4a8894d325a68e0a27b5b52736263f73f8245e28
AidenLx <aiden.lx@outlook.com>
2021-09-18 16:55:37 +0800

    chore: bump version to keep in sync with npm lib

--- src/const.ts
+++ src/const.ts
@@ -1,6 +1,6 @@
 import compareVersions from "compare-versions";
 
-const version = "3.0.0";
+const version = "3.0.2";
 export const VERSION: string = version;
 export const PREFIX = `<!--MN^${version}-->\n`;
 export const PREFIX_REGEX = /^<!--MN\^([\d.]+)-->\n/;
